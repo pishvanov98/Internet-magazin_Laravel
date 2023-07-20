57b20249004d7da137e6550b17c9c88a4592518f
nikita <nikita@aveldent.ru>
2023-07-20 18:01:47 +0300

    вывод главных категорий

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,12 +23,18 @@ class AppServiceProvider extends ServiceProvider
         $category = Category::with(['description'])->where('status',1)->get();
         $category_mass=[];
         foreach ($category as $key=> $item) {
-            $category_mass[]=$item->toArray();
-            $category_mass[$key]['href']=url('/category/'.$category_mass[$key]['category_id']);
+            $data=$item->toArray();
+           var_dump($data['parent_id']);
+            if($data['parent_id'] = 0) {
+
+                $category_mass[] = $data;
+                $category_mass[$key]['href'] = url('/category/' . $category_mass[$key]['category_id']);
+            }
+
         }
 
-        view()->composer('layouts.app', function ($view) use ($category_mass) {
-            $view->with('category', $category_mass);
+        view()->composer('layouts.app2', function ($view) use ($category_mass) {
+            $view->with('categories', $category_mass);
         });
     }
 }
