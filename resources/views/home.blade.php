d741382e0e26ad45572a78f2289beca5133a34da
nikita <nikita@aveldent.ru>
2023-07-20 11:43:41 +0300

    Revert "Регистрация"
    This reverts commit 768d6c2d1e09610a00c4a7ccfb1079bcf4c5f26e.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,17 +8,13 @@
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
 
-                    @guest
-
-                        Вы гость
-
-                    @else
-
-                        Вы авторизованы
-
-                    @endguest
-
+                    {{ __('You are logged in!') }}
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
 });
 
 Auth::routes();
 
-Route::get('/home', [\App\Http\Controllers\HomeController::class,'index'])->name('home');
-
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
