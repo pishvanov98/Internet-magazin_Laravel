768d6c2d1e09610a00c4a7ccfb1079bcf4c5f26e
nikita <nikita@aveldent.ru>
2023-07-20 11:38:53 +0300

    Регистрация

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,13 +8,17 @@
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
-                    {{ __('You are logged in!') }}
+                    @guest
+
+                        Вы гость
+
+                    @else
+
+                        Вы авторизованы
+
+                    @endguest
+
                 </div>
             </div>
         </div>
