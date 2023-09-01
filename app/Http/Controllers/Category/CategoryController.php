3d7f6128b15e17a5c71518a7f64fc3a4d6f0841f
nikita <pishvanov98@gmail.com>
2023-09-01 10:25:37 +0300

    Вывел дерево категорий в категории, дочерние элемениы и родителей

--- app/Http/Controllers/Category/CategoryController.php
+++ app/Http/Controllers/Category/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Category;
 
 use App\Components\ImageComponent;
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\CategoryDescription;
 use App\Models\Img;
 use Illuminate\Http\Request;
@@ -15,7 +16,7 @@ class CategoryController extends Controller
     {
 
         $category = CategoryDescription::where('slug', $request->route('slug'))->firstOrFail();
-
+        $CategoryTree=$this->categoryTree($category->category_id);
         $products_id_category=app('Search')->GetSearchAllProductToCategory($category->category_id);
         $page=0;
         $page = $request->get('page');
@@ -48,12 +49,49 @@ class CategoryController extends Controller
         }
 
 
-        return view('category.index',compact('images_slider','Products'));
+        return view('category.index',compact('images_slider','Products','CategoryTree'));
 
     }
 
 
     public function categoryTree($category_id){
-//необходимо получить дерево категории
+
+       $array_category_out=[];
+       $mainCategory = DB::connection('mysql2')->table('sd_category')->where('sd_category.category_id','=',$category_id)->where('sd_category.status','=',1)->select('sd_category.category_id','sd_category.parent_id','sd_category_description.name','sd_category_description.slug')
+       ->join('sd_category_description','sd_category_description.category_id','=','sd_category.category_id')->first();
+        $array_category_out[]=$mainCategory;
+        //ищем родителей категории
+       if(!empty($mainCategory->parent_id)){
+           $ParentCategory1 = DB::connection('mysql2')->table('sd_category')->where('sd_category.category_id','=',$mainCategory->parent_id)->where('sd_category.status','=',1)->select('sd_category.category_id','sd_category.parent_id','sd_category_description.name','sd_category_description.slug')
+               ->join('sd_category_description','sd_category_description.category_id','=','sd_category.category_id')->first();
+           if(empty($ParentCategory1->slug)){
+               $ParentCategory1->slug=app('Header')->SlugCategory($ParentCategory1->category_id);
+           }
+           $array_category_out[]=$ParentCategory1;
+
+           if(!empty($ParentCategory1->parent_id)){
+               $ParentCategory2 = DB::connection('mysql2')->table('sd_category')->where('sd_category.category_id','=',$ParentCategory1->parent_id)->where('sd_category.status','=',1)->select('sd_category.category_id','sd_category.parent_id','sd_category_description.name','sd_category_description.slug')
+                   ->join('sd_category_description','sd_category_description.category_id','=','sd_category.category_id')->first();
+               if(empty($ParentCategory2->slug)){
+                   $ParentCategory2->slug=app('Header')->SlugCategory($ParentCategory2->category_id);
+               }
+               $array_category_out[]=$ParentCategory2;
+           }
+
+       }
+       //ищем дочерние элементы категории
+        $array_category_out = array_reverse($array_category_out, true);
+        $Children = DB::connection('mysql2')->table('sd_category')->where('sd_category.parent_id','=',$mainCategory->category_id)->where('sd_category.status','=',1)->select('sd_category.category_id','sd_category.parent_id','sd_category_description.name','sd_category_description.slug')
+            ->join('sd_category_description','sd_category_description.category_id','=','sd_category.category_id')->get();
+        $Children->map(function ($item){
+            if(empty($item->slug)){
+                $item->slug=app('Header')->SlugCategory($ParentCategory2->category_id);
+            }
+            return $item;
+        });
+        $array_category_out['Children']=$Children->all();
+
+        return $array_category_out;
+
     }
 }
